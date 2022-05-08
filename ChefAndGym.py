a837829c2faeff189c365e8d82ab62c25c04f33f
imsaeef <iamsaifulislam23@gmail.com>
2022-05-09 00:13:04 +0600

    problemSolving

---
+++ ChefAndGym.py
@@ -0,0 +1,9 @@
+t = int(input())
+for _ in range(t):
+    x,y,z = map(int, input().split())
+    if (x+y)<=z:
+        print("2")
+    elif x<=z:
+        print("1")
+    else:
+        print("0")
---
+++ TheMangoTruck.py
@@ -0,0 +1,4 @@
+t = int(input())
+for _ in range(t):
+    x,y,z = map(int, input().split())
+    print((z-y)//x)
